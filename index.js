bc7a7be0fc477987205832dc7cffb405528432ae
Justice Marks <her@shedaddy.com>
2019-05-22 16:11:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function myAnimal(animal) {
 }
 
 function yourAnimal(animal) {
-  // note: this seems like cheating but it's the best way to make this 
+  // note: this seems like cheating but it's the best way to make this work in terms of scope
   animal = 'cat';
   // How can we make sure that this function
   // and the above function both pass?
